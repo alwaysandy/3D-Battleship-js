0bc5dffba9e366f42f5d5e7532997e26caaf24ad
Andrew Sas <andrewfsas@gmail.com>
2025-02-06 16:21:30 -0500

    Add very basic index, testing for room routing

--- server.js
+++ server.js
@@ -2,14 +2,22 @@ import express from "express";
 const app = express();
 const http = require('http').createServer(app);
 const io = require('socket.io')(http)
+const path = require('path');
 
+app.use(express.static('public'));
 
 io.on('connection', (socket) => {
     console.log("Testing");
 })
 
 app.get("/", (req, res) => {
-    res.send("Hello, world!");
+    res.sendFile(path.join(__dirname, 'public', 'place_ships.html'));
+});
+
+app.get("/room/:roomId", (req, res) => {
+   const room = req.params.roomId;
+   console.log(room);
+   res.send(`Entered room ${room}`);
 });
 
 const PORT = 8080;
